4d3b9a1a5e770fe5b20628082e2e3f302a6f3bb7
Len Smith <len@barrison.com>
2016-01-15 14:32:31 -0800

    Add CalendarWeekDecorator#events

--- app/decorators/calendar_week_decorator.rb
+++ app/decorators/calendar_week_decorator.rb
@@ -1,12 +1,18 @@
 # Take an array of events and returns an array that knows how many events are "
 
 class CalendarWeekDecorator < Draper::Decorator
-  Day = Struct.new(:name)
+  Day = Struct.new(:name, :events)
   DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
   delegate_all
 
   def days
-    @days ||= DAYS.map { |d| Day.new(d) }
+    @days ||= DAYS.map { |d| Day.new(d, events_for(d)) }
   end
+
+  private
+
+    def events_for(day_name)
+      object.find_all { |o| o[:day] == day_name }
+    end
 end
--- spec/decorators/calendar_week_decorator_spec.rb
+++ spec/decorators/calendar_week_decorator_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
 describe CalendarWeekDecorator do
-  let(:events)    { { }}
+  let(:events)   do
+    [
+      { name: 'Yoga', day: 'Sunday', start_time: '9:00', end_time: '10:00' },
+      { name: 'Running', day: 'Saturday', start_time: '11:00', end_time: '12:30' },
+      { name: 'Running', day: 'Saturday', start_time: '12:00', end_time: '13:30' },
+    ]
+  end
+
   let(:decorator) { CalendarWeekDecorator.new(events) }
+  let(:days)      { decorator.days }
 
   describe "days" do
     it "returns an collection of days" do
-      days = decorator.days.collect(&:name)
-      expect(days).to eq(["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"])
+      expect(days.collect(&:name)).to eq(["Sunday", "Monday",
+                                          "Tuesday", "Wednesday", "Thursday",
+                                          "Friday", "Saturday"])
+    end
+
+    it "assigns events to the correct day" do
+      expect(days.first.events.length).to eq(1)
+      expect(days.last.events.length).to eq(2)
     end
   end
 end
