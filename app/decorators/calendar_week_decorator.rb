8b1e33c379d95bb689aa2165ea4b29867f5aabc4
Len Smith <len@barrison.com>
2016-01-18 12:31:57 -0800

    Figure out what column an event belongs in

--- app/decorators/calendar_week_decorator.rb
+++ app/decorators/calendar_week_decorator.rb
@@ -10,41 +10,92 @@ class CalendarWeekDecorator < Draper::Decorator
     def initialize(name: name, events: events)
       self.name = name
       self.events = events
+
+      order_events
+      set_neighbors_and_columns
     end
 
     private
 
       def order_events
+        self.events.sort! { |e1, e2| e1.start_time <=> e2.start_time }
+      end
+
+      def set_neighbors_and_columns
+        self.events.each do |event|
+          neighbors = self.events.find_all do |other|
+            other != event && event.intersects?(other)
+          end
+          event.neighbors = neighbors.count
+          allocate_first_available_column(event, neighbors)
+        end
+      end
+
+      def allocate_first_available_column(event, neighbors)
+        taken_columns = neighbors.map(&:column).flatten
+
+        event.column = ((0..10).to_a - taken_columns).first
       end
   end
 
   class Event
-    attr_accessor :class_name, :name, :options, :others
+    attr_accessor :class_name, :name, :options
+
+    # how many other events are there at this time?
+    attr_accessor :neighbors
+
+    # which column does this go in
+    attr_accessor :column
 
     def initialize(options)
       self.name = options[:name]
       self.options = options
-      self.others = 0
     end
 
     def class_name
       "start-#{start_time} length-#{duration} others-0".gsub(/:/, "")
     end
 
-    private
+    def start_time
+      options[:start_time]
+    end
 
-      def start_time
-        options[:start_time]
-      end
+    def end_time
+      options[:end_time]
+    end
 
-      def end_time
-        options[:end_time]
-      end
+    def ==(other)
+      other.name == name && other.start_time == start_time && other.end_time && end_time
+    end
 
-      def duration
-        minutes = (end_time[0..1].to_i - start_time[0..1].to_i) * 60
-        minutes + (end_time[3..4].to_i - start_time[3..4].to_i)
-      end
+    def intersects?(other)
+      other.encompasses?(self) || encompasses?(other) || overlaps?(other)
+    end
+
+    def encompasses?(other)
+      start_time <= other.start_time && end_time >= other.end_time
+    end
+
+    def overlaps?(other)
+      overlaps_left_side(other) || overlaps_right_side(other)
+    end
+
+    def overlaps_right_side(other)
+      other.start_time <= start_time && other.end_time > start_time
+    end
+
+    def overlaps_left_side(other)
+      other.start_time < end_time && other.end_time >= end_time
+    end
+
+    def duration
+      minutes = (end_time[0..1].to_i - start_time[0..1].to_i) * 60
+      minutes + (end_time[3..4].to_i - start_time[3..4].to_i)
+    end
+
+    def to_s
+      name
+    end
   end
 
   delegate_all
--- spec/decorators/calendar_week_decorator_spec.rb
+++ spec/decorators/calendar_week_decorator_spec.rb
@@ -3,17 +3,18 @@ require 'spec_helper'
 describe CalendarWeekDecorator do
   let(:events)   do
     [
-      { name: 'Yoga', day: 'Sunday', start_time: '9:00', end_time: '10:00' },
-      { name: 'Running', day: 'Saturday', start_time: '11:00', end_time: '12:30' },
+      { name: 'Yoga',       day: 'Sunday',   start_time: '09:00', end_time: '10:00' },
+      { name: 'Soccer',     day: 'Saturday', start_time: '14:00', end_time: '15:30' },
+      { name: 'Running',    day: 'Saturday', start_time: '11:00', end_time: '12:30' },
       { name: 'Basketball', day: 'Saturday', start_time: '12:00', end_time: '21:30' },
-      { name: 'Soccer', day: 'Saturday', start_time: '14:00', end_time: '15:30' },
     ]
   end
 
   let(:decorator) { CalendarWeekDecorator.new(events) }
   let(:days)      { decorator.days }
+  let(:saturday)  { days.last}
 
-  describe "days" do
+  describe "#days" do
     it "returns an collection of days" do
       expect(days.collect(&:name)).to eq(["Sunday", "Monday",
                                           "Tuesday", "Wednesday", "Thursday",
@@ -29,9 +30,58 @@ describe CalendarWeekDecorator do
       expect(days.last.events.last.class_name).to match("start-1400 length-90 others-0")
     end
 
-    it "knows how many events overlap it" do
-      last = days.last.events.last
-      expect(last.neighbors).to eq 2
+    it "knows how to position events" do
+      running = saturday.events.first
+      expect(running.name).to eq "Running"
+
+      basketball = saturday.events.second
+      expect(basketball.name).to eq "Basketball"
+
+      soccer = saturday.events.last
+
+      expect(running.neighbors).to eq 1
+      expect(basketball.neighbors).to eq 2
+      expect(soccer.neighbors).to eq 1
+
+      expect(running.column).to eq 0
+      expect(basketball.column).to eq 1
+      expect(soccer.column).to eq 0
+    end
+  end
+
+  describe CalendarWeekDecorator::Event do
+    let(:klass)     { CalendarWeekDecorator::Event }
+    let(:morning)   { klass.new({ start_time: '09:00', end_time: '12:00' }) }
+    let(:long_am)   { klass.new({ start_time: '09:00', end_time: '13:00' }) }
+    let(:noon)      { klass.new({ start_time: '12:00', end_time: '13:00' }) }
+    let(:all_day)   { klass.new({ start_time: '08:00', end_time: '23:00' }) }
+    let(:late_pm)   { klass.new({ start_time: '12:30', end_time: '13:00' }) }
+
+    describe "#intersects?" do
+      it "true when the other event is encapsulated" do
+        expect(all_day.intersects?(morning)).to eq true
+      end
+
+      it "true when it encapsulates the other event" do
+        expect(morning.intersects?(all_day)).to eq true
+      end
+
+      it "true when they're the same time" do
+        expect(morning.intersects?(morning)).to eq true
+      end
+
+      it "true when the early side overlaps" do
+        expect(morning.intersects?(noon)).to eq false
+        expect(noon.intersects?(morning)).to eq false
+
+        expect(long_am.intersects?(noon)).to eq true
+        expect(noon.intersects?(long_am)).to eq true
+      end
+
+      it "true when the late side overlaps" do
+        expect(late_pm.intersects?(noon)).to eq true
+        expect(noon.intersects?(late_pm)).to eq true
+      end
     end
   end
 end
